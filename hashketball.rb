158afea2a1f6dce99ceb19c25d068c9c87c28811
Richard Chen <richard.c.chen@gmail.com>
2018-08-06 19:12:14 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -19,7 +19,11 @@ def game_hash
       :colors => ["Turquoise", "Purple"], 
       :players => {
         "Jeff Adrien" => {number: 4, shoe: 18, points: 10, rebounds: 1, assists: 1, steals: 2, blocks: 7, slam_dunks: 2 },
+<<<<<<< HEAD
         "Bismak Biyombo" => {number: 0, shoe: 16, points: 12, rebounds: 4, assists: 7, steals: 7, blocks: 15, slam_dunks: 10 },
+=======
+        "Bismak Biyombo" => {number: 0, shoe: 16, points: 12, rebounds: 4, assists: 7, steals: 7, blocks: 12, slam_dunks: 7 },
+>>>>>>> e07364a551a6c4906ce87d8697c6ebde63194494
         "DeSagna Diop" => {number: 2, shoe: 14, points: 24, rebounds: 12, assists: 12, steals: 4, blocks: 5, slam_dunks: 5 },
         "Ben Gordon" => {number: 8, shoe: 15, points: 33, rebounds: 3, assists: 2, steals: 1, blocks: 1, slam_dunks: 0 },
         "Brendan Haywood" => {number: 33, shoe: 15, points: 6, rebounds: 12, assists: 12, steals: 22, blocks: 5, slam_dunks: 12 } 
@@ -27,6 +31,7 @@ def game_hash
  }
 end
 
+<<<<<<< HEAD
 
 def num_points_scored(name)
   if game_hash[:home][:players].include?(name)
@@ -61,6 +66,35 @@ def team_names
   teamarray.push(game_hash[:away][:team_name])
   teamarray
 end
+=======
+def good_practices
+  game_hash.each do |location, team_data|
+    #are you ABSOLUTELY SURE what 'location' and 'team data' are? use binding.pry to find out!
+    binding.pry
+      team_data.each do |attribute, data|
+        #are you ABSOLUTELY SURE what 'attribute' and 'team data' are? use binding.pry to find out!
+        binding.pry
+ 
+        #what is 'data' at each loop through out .each block? when will the following line of code work and when will it break?
+        data.each do |data_item|
+            binding.pry
+      end
+    end
+  end
+end
+
+good_practices
+
+
+
+def num_points_scored(name)
+  points scored = 0
+  game_hash.each do |location, team_data|
+    team_data.each do |attribute, data|
+    end
+  end 
+end 
+>>>>>>> e07364a551a6c4906ce87d8697c6ebde63194494
 
 def player_numbers(teamname)
   jerseyarray = []
